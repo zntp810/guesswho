87be117bf672ad34b81dae61ed8394018bc585bf
zntp810 <Tranleanhtien0810@gmail.com>
2025-05-09 08:21:02 +0700

    add comment

--- znt810/trnt0810/main.cpp
+++ znt810/trnt0810/main.cpp
@@ -2,10 +2,10 @@
 
 int main() {
     weather::set_time(2);
-    thread t1(increase_time); t1.detach();
-    thread t2(update_weather); t2.detach();
-    thread t3(update_season); t3.detach();
-    thread t4(check_alive_by_time); t4.detach();
+    thread t1(increase_time); t1.detach(); // thread to increase time
+    thread t2(update_weather); t2.detach(); // thread to update weather
+    thread t3(update_season); t3.detach(); // thread to update season
+    thread t4(check_alive_by_time); t4.detach(); // thread to check if the plant is alive every 1 second
 
     display x;
     x.show_scr();
